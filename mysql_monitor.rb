effd897cce7604a2d79e14c0d5b5493fc3f53802
Benjamin Nørgaard <benjamin@webtoad.dk>
2015-11-19 13:05:09 +0100

    Use Proccess module to determine if we are root

--- mysql_monitor.rb
+++ mysql_monitor.rb
@@ -15,7 +15,7 @@ class MysqlMonitor
   end
 
   def check_if_root
-    if ENV['USER'] != 'root'
+    if Process.uid != 0
       puts 'You need root privileges to run this script'
       exit 1
     end
